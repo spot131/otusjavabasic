d73d97b3ad5c076e9ac490499457c15a1f1f45dd
RD <spot131@gmail.com>
2024-10-12 11:34:54 +0300

    hw1

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework1/MainApp.java
@@ -0,0 +1,15 @@
+package ru.otus.java.basic.homeworks.homework1;
+
+public class MainApp
+{
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings(){
+        System.out.println("Hello");
+        System.out.println("world");
+        System.out.println("from");
+        System.out.println("java");
+    }
+}
