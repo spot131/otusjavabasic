eb17c1dcff499e144781a3dcadeefb18adfd89b3
RD <spot131@gmail.com>
2024-11-21 14:29:03 +0300

    in progress

--- src/main/java/ru/otus/java/basic/homeworks/interfaces/MainApp.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.homeworks.interfaces;
-
-public class MainApp {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/ru/otus/java/basic/homeworks/training/Calendar.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.java.basic.homeworks.training;
-
-public class Calendar {
-    public DayOfWeek dayOfWeek;
-
-    public Calendar(DayOfWeek dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
-    }
-}
