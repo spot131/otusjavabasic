2f0a703901e095c62fc936c6de916512d77ef69f
RD <spot131@gmail.com>
2024-11-30 14:27:25 +0300

    in progress

--- src/main/java/ru/otus/java/basic/homeworks/homework9/MainApp.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/MainApp.java
@@ -1,4 +1,16 @@
 package ru.otus.java.basic.homeworks.homework9;
 
+import java.util.ArrayList;
+
 public class MainApp {
+    public static void main(String[] args) {
+    return;
+    }
+    public void returnSequential (int min, int max) {
+         ArrayList<Integer> list = new ArrayList<>();
+        for (int i = min; i < max; i++) {
+            list.add(i);
+
+        }
+    }
 }
