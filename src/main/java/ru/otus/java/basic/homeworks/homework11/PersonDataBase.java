b7931cde0f19232fdd6927478457ed3bb15d2d13
RD <spot131@gmail.com>
2025-01-20 19:35:52 +0300

    Разбор алгоритмов \\ ДЗ выполнено

--- src/main/java/ru/otus/java/basic/homeworks/homework11/PersonDataBase.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework11/PersonDataBase.java
@@ -1,4 +1,48 @@
 package ru.otus.java.basic.homeworks.homework11;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersonDataBase {
+    private Map<Long, Person> personMap = new HashMap<>();
+
+    public Person findById(Long id) {
+        return personMap.get(id);
+    }
+
+    public void add(Person person) {
+        personMap.put(person.id, person);
+    }
+
+    public boolean isManager(Person person) {
+        if (person == null || person.position == null) {
+            return false;
+        }
+        switch (person.position) {
+            case MANAGER:
+            case DIRECTOR:
+            case BRANCH_DIRECTOR:
+            case SENIOR_MANAGER:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean isEmployee(Long id) {
+        Person person = findById(id);
+        if (person == null || person.position == null) {
+            return false;
+        }
+
+        switch (person.position) {
+            case MANAGER:
+            case DIRECTOR:
+            case BRANCH_DIRECTOR:
+            case SENIOR_MANAGER:
+                return false;
+            default:
+                return true;
+        }
+    }
 }
